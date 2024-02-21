c5bf29cc7185f78f4c4dede9d345efced651bc2d
Chisaki Ibe <chisaki@ChisakinoMacBook-Pro.local>
2024-02-21 21:41:44 +0900

    add period select

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import axios from 'axios';
 import LineChart from './LineChart';
 import { Chart, registerables } from 'chart.js';
-import { format } from 'date-fns';
+import { format, subMonths } from 'date-fns';
 import './App.css';
 
 Chart.register(...registerables);
@@ -15,14 +15,20 @@ function App() {
   const [stockData, setStockData] = useState({});
   const [stockPrice, setStockPrice] = useState({});
   const [stockMonthData, setStockMonthData] = useState(null);
+  const [isShowPeriodList, setShowPeriodList] = useState(false)
 
   const onChangeStockSymbol = (event) => setStockSymbol(event.target.value);
 
   const handleClick = () => {
     getStockData(stockSymbol);
     getStockPrice(stockSymbol);
-    getStockMonthData(stockSymbol);
+    getStockPeriodData(stockSymbol, 1);
+    setShowPeriodList(true);
   };
+
+  const onChangeStockPeriodDate = (period) => {
+    getStockPeriodData(stockSymbol, period);
+  }
 
   const getStockData = (symbol) => {
     axios.get(`${URL}/tickers/${symbol}?access_key=${ACCESS_KEY}`)
@@ -38,10 +44,10 @@ function App() {
       })
   }  
 
-  const getStockMonthData = async (symbol) => {
+  const getStockPeriodData = async (symbol, period) => {
     let data = [];
     let labels = [];
-    await axios.get(`${URL}/eod?access_key=${ACCESS_KEY}&symbols=${symbol}&date_from=2024-01-17&date_to=2024-02-16&sort=ASC`)
+    await axios.get(`${URL}/eod?access_key=${ACCESS_KEY}&symbols=${symbol}&date_from=${format(subMonths(new Date(), period), 'yyyy-MM-dd')}&date_to=${format(new Date(), 'yyyy-MM-dd')}&sort=ASC`)
       .then(response => {
         for ( let stock of response.data.data){
           data.push(stock.close)
@@ -57,7 +63,7 @@ function App() {
         }
       ]
     })
-  }    
+  } 
 
   return (
     <div className="App">
@@ -76,6 +82,24 @@ function App() {
           <button onClick={handleClick}>SEARCH</button>
         </div>
       </div>
+      {isShowPeriodList && 
+      <div>
+        <input
+          type="radio"
+          name="period"
+          onChange={() => onChangeStockPeriodDate(1)}
+        /> 1ヶ月
+        <input
+          type="radio"
+          name="period"
+          onChange={() => onChangeStockPeriodDate(6)}
+        /> 半年
+        <input
+          type="radio"
+          name="period"
+          onChange={() => onChangeStockPeriodDate(12)}
+        /> 1年        
+      </div>}    
       <div className='Chart'>
         <LineChart stockMonthData={stockMonthData} />
       </div>
