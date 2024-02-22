00fa36a95fc7dda068bffc7dd9a3cd7696a8aad1
Chisaki Ibe <chisaki@ChisakinoMacBook-Pro.local>
2024-02-23 00:42:25 +0900

    run prettier

--- src/App.js
+++ src/App.js
@@ -8,82 +8,87 @@ import './App.scss';
 Chart.register(...registerables);
 
 function App() {
-  const URL = "http://api.marketstack.com/v1";
+  const URL = 'http://api.marketstack.com/v1';
   const ACCESS_KEY = `${process.env.REACT_APP_STOCK_CHART_API}`;
 
   const [stockSymbol, setStockSymbol] = useState('');
   const [stockData, setStockData] = useState({});
   const [stockPrice, setStockPrice] = useState({});
   const [stockMonthData, setStockMonthData] = useState(null);
-  const [isShow, setShow] = useState(false)
+  const [isShow, setShow] = useState(false);
   const [errorMessage, setErrorMessage] = useState('');
 
   const getStockData = (symbol) => {
-    axios.get(`${URL}/tickers/${symbol}?access_key=${ACCESS_KEY}`)
-      .then(response => {
-        setStockData(response.data)
+    axios
+      .get(`${URL}/tickers/${symbol}?access_key=${ACCESS_KEY}`)
+      .then((response) => {
+        setStockData(response.data);
       })
-      .catch(error => {
+      .catch((error) => {
         console.error('Failed to fetch stock data', error.message);
         setErrorMessage('株式データの取得に失敗しました');
         setShow(false);
       });
-  }
+  };
 
   const getStockPrice = (symbol) => {
-    axios.get(`${URL}/tickers/${symbol}/eod/latest?access_key=${ACCESS_KEY}`)
-      .then(response => {
+    axios
+      .get(`${URL}/tickers/${symbol}/eod/latest?access_key=${ACCESS_KEY}`)
+      .then((response) => {
         setStockPrice(response.data);
       })
-      .catch(error => {
-        console.error('Failed to fetch stock data', error.message);        
+      .catch((error) => {
+        console.error('Failed to fetch stock data', error.message);
         setErrorMessage('株式データの取得に失敗しました');
         setShow(false);
       });
-  }  
+  };
 
   const getStockPeriodData = async (symbol, period) => {
     let data = [];
     let labels = [];
-    await axios.get(`${URL}/eod?access_key=${ACCESS_KEY}&symbols=${symbol}&date_from=${period}&date_to=${format(new Date(), 'yyyy-MM-dd')}&limit=1000&sort=ASC`)
-      .then(response => {
-        for ( let stock of response.data.data){
-          data.push(stock.close)
-          labels.push(format(stock.date, 'MM/dd/yyyy'))
+    await axios
+      .get(
+        `${URL}/eod?access_key=${ACCESS_KEY}&symbols=${symbol}&date_from=${period}&date_to=${format(new Date(), 'yyyy-MM-dd')}&limit=1000&sort=ASC`,
+      )
+      .then((response) => {
+        for (let stock of response.data.data) {
+          data.push(stock.close);
+          labels.push(format(stock.date, 'MM/dd/yyyy'));
         }
-        })
-        .catch(error => {
-          console.error('Failed to fetch stock data', error.message);
-          setErrorMessage('株式データの取得に失敗しました');
-        });
+      })
+      .catch((error) => {
+        console.error('Failed to fetch stock data', error.message);
+        setErrorMessage('株式データの取得に失敗しました');
+      });
     setStockMonthData({
       labels: labels,
-      datasets:[
+      datasets: [
         {
           borderColor: 'rgba(35,200,153,1)',
           data: data,
-        }
-      ]
-    })
-  }
+        },
+      ],
+    });
+  };
 
   const onChangeStockSymbol = (event) => setStockSymbol(event.target.value);
 
   const getStockDayPeriodData = (symbol, period) => {
-    const subDaysDate = format(subDays(new Date(), period), 'yyyy-MM-dd')
-    getStockPeriodData(symbol, subDaysDate)
-  }  
+    const subDaysDate = format(subDays(new Date(), period), 'yyyy-MM-dd');
+    getStockPeriodData(symbol, subDaysDate);
+  };
 
   const getStockMonthPeriodData = (symbol, period) => {
-    const subMonthsDate = format(subMonths(new Date(), period), 'yyyy-MM-dd')
-    getStockPeriodData(symbol, subMonthsDate)
-  }
+    const subMonthsDate = format(subMonths(new Date(), period), 'yyyy-MM-dd');
+    getStockPeriodData(symbol, subMonthsDate);
+  };
 
   const handleClick = () => {
     for (const element of document.getElementsByName('period')) {
       element.checked = false;
     }
-    
+
     getStockData(stockSymbol);
     getStockPrice(stockSymbol);
     getStockMonthPeriodData(stockSymbol, 1);
@@ -92,29 +97,54 @@ function App() {
 
   const onChangeStockDayPeriodDate = (period) => {
     getStockDayPeriodData(stockSymbol, period);
-  }  
+  };
 
   const onChangeStockMonthPeriodDate = (period) => {
     getStockMonthPeriodData(stockSymbol, period);
-  }
+  };
 
   return (
     <>
-      {(errorMessage && isShow === false) && <div className="ErrorMessage"><p>{errorMessage}</p></div>}
+      {errorMessage && isShow === false && (
+        <div className="ErrorMessage">
+          <p>{errorMessage}</p>
+        </div>
+      )}
       <div className="App">
         <div className="Header">
-          {isShow &&
-          <div className="StockData">
-            <h1>{stockData.name}</h1>          
-            <ul className="StockDataList">
-              <li>始値<br /><b>{stockPrice.open}</b></li>
-              <li>高値<br /><b>{stockPrice.high}</b></li>
-              <li>安値<br /><b>{stockPrice.low}</b></li>
-              <li>終値<br /><b>{stockPrice.close}</b></li>
-              <li>出来高<br /><b>{stockPrice.volume}</b></li>
-            </ul>
-            <p>※ 現地時刻の前日のデータを取得しています</p>
-          </div>}
+          {isShow && (
+            <div className="StockData">
+              <h1>{stockData.name}</h1>
+              <ul className="StockDataList">
+                <li>
+                  始値
+                  <br />
+                  <b>{stockPrice.open}</b>
+                </li>
+                <li>
+                  高値
+                  <br />
+                  <b>{stockPrice.high}</b>
+                </li>
+                <li>
+                  安値
+                  <br />
+                  <b>{stockPrice.low}</b>
+                </li>
+                <li>
+                  終値
+                  <br />
+                  <b>{stockPrice.close}</b>
+                </li>
+                <li>
+                  出来高
+                  <br />
+                  <b>{stockPrice.volume}</b>
+                </li>
+              </ul>
+              <p>※ 現地時刻の前日のデータを取得しています</p>
+            </div>
+          )}
           <div className="StockSearch">
             <div className="StockSearch__Form">
               <input
@@ -127,72 +157,55 @@ function App() {
               <button onClick={handleClick}></button>
             </div>
             <div className="StockSearch__Text">
-              <p>銘柄のリストは <a href="https://marketstack.com/search" target="_blank">こちら</a> からご参照ください<br />Symbol をご入力ください</p>
+              <p>
+                銘柄のリストは{' '}
+                <a href="https://marketstack.com/search" target="_blank">
+                  こちら
+                </a>{' '}
+                からご参照ください
+                <br />
+                Symbol をご入力ください
+              </p>
             </div>
           </div>
         </div>
-        {isShow &&
-        <div className="StockChartPeriod">
-          <label>
-            <input
-              type="radio"
-              name="period"
-              onChange={() => onChangeStockDayPeriodDate(1)}
-            /> 1日
-          </label>
-          <label>
-            <input
-              type="radio"
-              name="period"
-              onChange={() => onChangeStockDayPeriodDate(7)}
-            /> 1週
-          </label>                
-          <label>
-            <input
-              type="radio"
-              name="period"
-              onChange={() => onChangeStockMonthPeriodDate(1)}
-            /> 1ヶ月
-          </label>
-          <label>
-            <input
-              type="radio"
-              name="period"
-              onChange={() => onChangeStockMonthPeriodDate(3)}
-            /> 3ヶ月
-          </label>
-          <label>
-            <input
-              type="radio"
-              name="period"
-              onChange={() => onChangeStockMonthPeriodDate(6)}
-            /> 6ヶ月
-          </label>
-          <label>
-            <input
-              type="radio"
-              name="period"
-              onChange={() => onChangeStockMonthPeriodDate(12)}
-            /> 1年
-          </label>
-          <label>
-            <input
-              type="radio"
-              name="period"
-              onChange={() => onChangeStockMonthPeriodDate(24)}
-            /> 2年
-          </label>
-          <label>
-            <input
-              type="radio"
-              name="period"
-              onChange={() => onChangeStockMonthPeriodDate(36)}
-            /> 3年
-          </label>
-        </div>}
-        <div className="Chart">
-          {stockMonthData && <Line data={stockMonthData} />}
-        </div>
+        {isShow && (
+          <div className="StockChartPeriod">
+            <label>
+              <input type="radio" name="period" onChange={() => onChangeStockDayPeriodDate(1)} />{' '}
+              1日
+            </label>
+            <label>
+              <input type="radio" name="period" onChange={() => onChangeStockDayPeriodDate(7)} />{' '}
+              1週
+            </label>
+            <label>
+              <input type="radio" name="period" onChange={() => onChangeStockMonthPeriodDate(1)} />{' '}
+              1ヶ月
+            </label>
+            <label>
+              <input type="radio" name="period" onChange={() => onChangeStockMonthPeriodDate(3)} />{' '}
+              3ヶ月
+            </label>
+            <label>
+              <input type="radio" name="period" onChange={() => onChangeStockMonthPeriodDate(6)} />{' '}
+              6ヶ月
+            </label>
+            <label>
+              <input type="radio" name="period" onChange={() => onChangeStockMonthPeriodDate(12)} />{' '}
+              1年
+            </label>
+            <label>
+              <input type="radio" name="period" onChange={() => onChangeStockMonthPeriodDate(24)} />{' '}
+              2年
+            </label>
+            <label>
+              <input type="radio" name="period" onChange={() => onChangeStockMonthPeriodDate(36)} />{' '}
+              3年
+            </label>
+          </div>
+        )}
+        <div className="Chart">{stockMonthData && <Line data={stockMonthData} />}</div>
       </div>
     </>
   );
